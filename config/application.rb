cb921c58fbfc29943fc5b5f6ef1ea96042951041
BilionBoy <dhiogoandre26@gmail.com>
2025-01-21 20:38:24 -0400

    WIP

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module MenuxT
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+    config.paths["lib/templates/erb/scaffold"] = "lib/templates/erb/scaffold"
+
     config.time_zone = "La Paz" # Configuração do Time Zone
 
     config.i18n.default_locale = :'pt-BR' # Configuração do idioma padrão
--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,4 +1,4 @@
 require "pagy/extras/i18n"
 require "pagy/extras/bootstrap"
 
-Pagy::DEFAULT[:items] = 7
+Pagy::DEFAULT[:items] = 6
