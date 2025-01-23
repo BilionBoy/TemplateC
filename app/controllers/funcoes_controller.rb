863edfdf9597bba745ff477829852e497d1eca61
Dhiogo <dhiogoandre26@gmail.com>
2025-01-23 12:51:57 -0400

    Devise: Configuração layout

--- app/controllers/funcoes_controller.rb
+++ app/controllers/funcoes_controller.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 class FuncoesController < ApplicationController
   before_action :set_funcao, only: %i[show edit update destroy]
 
@@ -6,7 +7,7 @@ class FuncoesController < ApplicationController
 
  def index
     @q = Funcao.ransack(params[:q])
-    @pagy, @funcoes = pagy(@q.result, limit:7)
+    @pagy, @funcoes = pagy(@q.result, limit: 7)
   end
 
   def new
@@ -20,7 +21,7 @@ class FuncoesController < ApplicationController
     @funcao = Funcao.new(funcao_params)
 
     if @funcao.save
-      redirect_to funcoes_path, notice: t('messages.created_successfully')
+      redirect_to funcoes_path, notice: t("messages.created_successfully")
     else
       render :new, status: :unprocessable_entity
     end
@@ -28,7 +29,7 @@ class FuncoesController < ApplicationController
 
   def update
     if @funcao.update(funcao_params)
-      redirect_to funcoes_path, notice: t('messages.updated_successfully'), status: :see_other
+      redirect_to funcoes_path, notice: t("messages.updated_successfully"), status: :see_other
     else
       render :edit, status: :unprocessable_entity
     end
@@ -36,25 +37,25 @@ class FuncoesController < ApplicationController
 
   def destroy
     if @funcao.destroy
-      redirect_to funcoes_url, notice: t('messages.deleted_successfully')
+      redirect_to funcoes_url, notice: t("messages.deleted_successfully")
     else
-      redirect_to funcoes_url, alert: t('messages.delete_failed_due_to_dependencies')
-    end   
+      redirect_to funcoes_url, alert: t("messages.delete_failed_due_to_dependencies")
+    end
   end
 
   private
 
   def set_funcao
     @funcao = Funcao.find_by(id: params[:id])
-    return redirect_to funcoes_path, alert: t('messages.not_found') unless @funcao
+    redirect_to funcoes_path, alert: t("messages.not_found") unless @funcao
   end
 
   def funcao_params
-    permitted_attributes = Funcao.column_names.reject { |col| ['deleted_at', 'created_by', 'updated_by'].include?(col) }
+    permitted_attributes = Funcao.column_names.reject { |col| [ "deleted_at", "created_by", "updated_by" ].include?(col) }
     params.require(:funcao).permit(permitted_attributes.map(&:to_sym))
   end
 
   def handle_not_found
-    redirect_to funcoes_path, alert: t('messages.not_found')
+    redirect_to funcoes_path, alert: t("messages.not_found")
   end
 end
