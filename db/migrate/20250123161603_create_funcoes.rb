c544d150cf3e635ffe5adfad1fbc4b8a8962eb31
Dhiogo <dhiogoandre26@gmail.com>
2025-01-23 12:31:58 -0400

    Devise: install

--- db/migrate/20250123161603_create_funcoes.rb
+++ db/migrate/20250123161603_create_funcoes.rb
@@ -4,7 +4,7 @@ class CreateFuncoes < ActiveRecord::Migration[7.2]
   def up
     create_table :funcoes do |t|
       t.string :nome
-          
+
       t.string :created_by
       t.string :updated_by
       t.datetime :deleted_at
