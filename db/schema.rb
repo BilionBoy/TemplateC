50796a818ebbba0787489f2b2723814b4bebfdae
Dhiogo <dhiogoandre26@gmail.com>
2025-01-23 12:18:05 -0400

    scaffold: funcao

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_23_155153) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_23_161603) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "funcoes", force: :cascade do |t|
+    t.string "nome"
+    t.string "created_by"
+    t.string "updated_by"
+    t.datetime "deleted_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
