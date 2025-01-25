a428c63f720f6b409d52ed93bb615a9f7c462915
BilionBoy <dhiogoandre26@gmail.com>
2025-01-25 01:41:59 -0400

    WIP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!  # Autentica o usuário antes de qualquer ação
   include Pagy::Backend  # Inclui Pagy para uso no Backend
-  allow_browser versions: :modern
 
   # Definir o layout de acordo com o controller
   before_action :set_layout_by_controller
